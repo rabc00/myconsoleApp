06699cea16949fc97c3bc2b524bb2a1da6400bcf
rabc00 <RBOLAN2207@suscorp.edu>
2016-07-12 19:50:18 -0400

    new update
    txt write and class creation

--- myconsoleApp/Program.cs
+++ myconsoleApp/Program.cs
@@ -6,28 +6,45 @@ using System.Threading.Tasks;
 
 namespace myconsoleApp
 {
+    public class User
+    {
+        public string Name; public string phoneN; public string email;
+
+
+    }
+
+
+
+
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            string user_input = "";
+
+            User user_object = new User();
+
+
+           // string user_input = "";
             string inputS = "";
-            string email = "";
-            string phoneN = "";
+            //string email = "";
+            //string phoneN = "";
             string response = "";
             Console.WriteLine("Hello World");
-            user_input = Console.ReadLine();
-            Console.WriteLine("Hello " + user_input);
+            user_object.Name = Console.ReadLine();
+            Console.WriteLine("Hello " + user_object.Name);
             inputS = Console.ReadLine();
             
             if (inputS == "who are you")
             {
                 Console.WriteLine("I am Emma");
                 Console.WriteLine("What is your email?");
-                email = Console.ReadLine();
+                user_object.email = Console.ReadLine();
                 Console.WriteLine("What is your phone number?");
-                phoneN = Console.ReadLine();
-                Console.WriteLine("So you Are " + user_input + " Your email is " + email + " and Your phone number is " + phoneN);
+                user_object.phoneN = Console.ReadLine();
+                Console.WriteLine("So you Are " + user_object.Name + " Your email is " + user_object.email + " and Your phone number is " + user_object.phoneN);
                 Console.WriteLine("Are you my admin?");
                 response = Console.ReadLine();
                 if (response == "yes")
@@ -52,7 +69,7 @@ namespace myconsoleApp
                     Console.WriteLine("Press any key to exit");
                     Console.ReadLine();
                 }
-            }
+            }//If InputS Terminates
             else 
             {
                 Console.WriteLine("You did not ask who i am. Piss Off Jerk ");
@@ -61,6 +78,20 @@ namespace myconsoleApp
 
             }
 
-        }
+            user_object.Name = "Name: " + user_object.Name + "; ";
+            user_object.phoneN = "Phone Number: " + user_object.phoneN + "; ";
+            user_object.email = "E-Mail: " + user_object.email + "; ";
+
+           /* string[] lines = {user_object.Name, user_object.phoneN, user_object.email};
+
+            System.IO.File.WriteAllLines(@"E:\aspnet\myconsoleApp\Records.txt", lines);
+            */
+
+
+
+
+
+
+        }//Main Ends
     }
 }
